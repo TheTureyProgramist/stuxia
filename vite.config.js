b54d971fcd9a5bfac388749c782cd757db5bee03
TheTureyProgramist <150546608+TheTurkeyProgramist@users.noreply.github.com>
2026-08-27 14:14:23 -0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  esbuild: {
-    loader: 'jsx',
-    include: /src\/.*\.jsx?$/,
-    exclude: [],
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx',
-      },
-    },
-  },
+  base: './',
   define: {
     'process.env': {},
   },
